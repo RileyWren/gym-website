2197aa22556e63a769ae8ec4505c00b94fb4c124
xcx-7 <zainab.42847@gmail.com>
2025-05-26 14:16:54 +0600

    Updated

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -9,11 +9,17 @@ const Offer = () => {
   return (
     <div className='h-screen flex flex-col bg-neutral-950 text-white font-extrabold text-center items-center'>
         <h1 className='text-4xl m-20 sm:text-5xl md:m-30'>We offer something for <br className='hidden md:block'/> everybody</h1>
-        <img src={gym1} />
-        <img src={gym2} />
-        <img src={gym3} />
-        <img src={gym4} />
-
+<div className='grid w-full gap-4 px-4 m-15
+  grid-cols-1 
+  sm:grid-cols-2 
+  md:grid-cols-3 
+  lg:grid-cols-4
+'>
+  <img src={gym1} className='w-full h-43 object-cover rounded-lg' />
+  <img src={gym2} className='w-full h-43 object-cover rounded-lg' />
+  <img src={gym3} className='w-full h-43 object-cover rounded-lg' />
+  <img src={gym4} className='w-full h-43 object-cover rounded-lg' />
+</div>
     </div>
   )
 }
