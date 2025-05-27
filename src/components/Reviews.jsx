a71f0909f906edf8d6eaf6958607dcc65dd83768
xcx-7 <zainab.42847@gmail.com>
2025-05-27 06:09:12 +0600

    Updated

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -5,18 +5,18 @@ import Button from './Button'
 
 const Reviews = () => {
   return (
-    <div>
-        <h4>Membership</h4>
-        <h2>Crushing your health and fitness goals starts here...</h2>
+    <div className='flex flex-col items-center h-screen bg-[#5f7252] text-white'>
+        <h4 className='text-2xl m-10 border-2 border-white rounded-full px-4 py-2 text-sm sm:text-base md:text-lg lg:text-xl w-fit mb-9'>Membership</h4>
+        <h2 className='text-6xl max-w-4xl font-bold text-center m-5'>Crushing your health and fitness goals starts here...</h2>
         <div>
-            <div>
-                <h3>Month to Month</h3>
-                <h3>$165</h3>/mo
+            <div className='bg-white text-black text-center rounded-4xl m-10 p-10 text-2xl/18'>
+                <h3 className='font-bold text-4xl mb-10'>Month to <br /> Month</h3>
+                <h3><strong className='text-[#5f7252] font-extrabold text-5xl'>$165</strong>/mo</h3>
                
-                <FontAwesomeIcon icon={faCircleCheck} className='text-green-700' /> All classes
-                 <FontAwesomeIcon icon={faCircleCheck} className='text-green-700' /> All member events
-                  <FontAwesomeIcon icon={faCircleCheck} className='text-green-700' /> Full gym access
-                  <p>Charges every month unless you cancel</p>
+                <FontAwesomeIcon icon={faCircleCheck} className='text-[#5f7252]' /> All classes <br />
+                 <FontAwesomeIcon icon={faCircleCheck} className='text-[#5f7252]' /> All member events <br />
+                  <FontAwesomeIcon icon={faCircleCheck} className='text-[#5f7252]' /> Full gym access
+                  <p className='text-gray-500'>Charges every month <br /> unless you cancel</p>
                 <Button content="Start 7 day free trial" bgColor="black" hoverColor="black" textColor="#ffffff" />
             </div>
             <div></div>
