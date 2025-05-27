a02f9fe0c8cb03f7067770f3e71da3dd22070080
xcx-7 <zainab.42847@gmail.com>
2025-05-27 09:05:48 +0600

    Updated

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Rating from './Rating'
+import man from '../assets/man.jpeg'
+import man2 from '../assets/man2.jpeg'
+import man3 from '../assets/man3.webp'
+
 
 const Reviews = () => {
   return (
     <div>
-      
+      <div>
+        <Rating />
+        <img src={man} className='rounded-full h-6'  />
+      </div>
     </div>
   )
 }
